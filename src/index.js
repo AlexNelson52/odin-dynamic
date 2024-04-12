b01e4d030b59cdfe39ef324be14e80370a6a964b
Alex N <77512968+AlexNelson52@users.noreply.github.com>
2024-04-11 20:55:16 -0400

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
-import generateJoke from "./gnerateJoke";
-import './styles/main.scss';
-import laughing from './assets/laughing.svg';
+import "./styles/style.css";
 
-const laughImg = document.getElementById('laughImg');
-laughImg.src = laughing;
-console.log(generateJoke());
+// terminal \\
 
-const jokeBtn = document.getElementById('jokeBtn');
-jokeBtn.addEventListener('click',generateJoke);
+// npm init -y
+// npm install webpack webpack-cli --save-dev
 
-generateJoke();
+//To bundle the lodash dependency with index.js, we'll need to install the library locally:
+//npm install --save lodash
+
+// ADD  "build": "webpack" to package.json
+
+//npm install --save-dev @babel/core
+
+//npm install --save-dev html-webpack-plugin
+
+//npm install --save-dev style-loader css-loader
+
+// npm install --save-dev webpack-dev-server
