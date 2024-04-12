7116d8d021e0311493c3b1a5982cc503e6744ef8
Alex N <77512968+AlexNelson52@users.noreply.github.com>
2024-04-11 20:57:30 -0400

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,5 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const BundleAnalyzerPlugin =
-  require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   mode: "development",
@@ -28,19 +26,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.scss$/,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
+
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
@@ -53,6 +42,5 @@ module.exports = {
       filename: "index.html",
       template: "src/template.html",
     }),
-    // new BundleAnalyzerPlugin(),
   ],
 };
