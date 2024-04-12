4772c3e6f5cdd5aa7b66fc9fce3eab658ad7cc67
Alex N <77512968+AlexNelson52@users.noreply.github.com>
2024-04-11 20:55:49 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+import "./styles/style.css";
+
+// terminal \\
+
+// npm init -y
+// npm install webpack webpack-cli --save-dev
+
+//To bundle the lodash dependency with index.js, we'll need to install the library locally:
+//npm install --save lodash
+
+// ADD  "build": "webpack" to package.json
+
+//npm install --save-dev @babel/core
+
+//npm install --save-dev html-webpack-plugin
+
+//npm install --save-dev style-loader css-loader
+
+// npm install --save-dev webpack-dev-server
